5d18b484bb105214184013b4549b765441ca57fd
ShriyaDesaiReddy <112955102+ShriyaDesaiReddy@users.noreply.github.com>
2022-11-16 19:51:39 +0530

    Update todo.js

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -1,40 +1,37 @@
-let todoList = require("../todo");
-
-const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
-/* eslint-disable no-undef */
-describe("Todo List", () => {
-  beforeAll(() => {
-    const today = new Date();
-    const oneDay = 60 * 60 * 24 * 1000;
+let todoList=require("../todo");
+const{all,markAsComplete,add,overdue,dueToday,dueLater}=todoList();
+describe("Todo List",()=>{
+  beforeAll(()=>{
+    const today=new Date();
+    const oneDay=60*60*24*1000;
     [
       {
-        title: "Buy Bread",
-        completed: false,
-        dueDate: new Date(today.getTime() - 2 * oneDay).toLocaleDateString(
+        title:"Buy Bread",
+        completed:false,
+        dueDate:new Date(today.getTime()-2*oneDay).toLocaleDateString(
           "en-CA"
         ),
       },
       {
-        title: "Pay Mobile bill",
-        completed: false,
-        dueDate: new Date().toLocaleDateString("en-CA"),
+        title:"Pay Mobile bill",
+        completed:false,
+        dueDate:new Date().toLocaleDateString("en-CA"),
       },
       {
-        title: "Write assignment",
-        completed: false,
-        dueDate: new Date(today.getTime() + 2 * oneDay).toLocaleDateString(
+        title:"Write assignment",
+        completed:false,
+        dueDate:new Date(today.getTime()+2*oneDay).toLocaleDateString(
           "en-CA"
         ),
       },
     ].forEach(add);
   });
-  test("Should add a new todo", () => {
+  test("Should add a new todo",()=>{
     expect(all.length).toEqual(3);
-
     add({
-      title: "Do test item",
-      completed: false,
-      dueDate: new Date().toLocaleDateString("en-CA"),
+      title:"Do test item",
+      completed:false,
+      dueDate:new Date().toLocaleDateString("en-CA"),
     });
 
     expect(all.length).toEqual(4);
